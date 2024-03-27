d2f8d79e22f729513d1e1b1c93cd123a19a0cba6
v.janikashvili <vjanikashvili@gmail.com>
2024-03-27 20:09:04 +0400

    productapi2

---
+++ backend/productApi2/Controllers/ProductController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductApi2.Data;
+using ProductApi2.Models;
+
+namespace productApi2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductDbContext _dbContext;
+
+        public ProductController(ProductDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var products = await _dbContext.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> getById(int id)
+        {
+            var product = await _dbContext.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Products.Add(product);
+                await _dbContext.SaveChangesAsync();
+
+                return Ok(product);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> delete(int id)
+        {
+            var product = await _dbContext.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Products.Remove(product);
+            await _dbContext.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> put(int id, Product updateProduct)
+        {
+            var existingProduct = await _dbContext.Products.FindAsync(id);
+
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+
+            existingProduct.Name = updateProduct.Name;
+            existingProduct.Price = updateProduct.Price;
+
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ backend/productApi2/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ProductApi2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; } 
+
+        public decimal Price { get; set; }
+    }
+}
